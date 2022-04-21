207fc5e8f1b14af90f891f420d8c38d7ef9f8416
MicheleMallia <mallia.michele@gmail.com>
2022-04-21 16:21:45 +0200

    added views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { AboutComponent } from './views/about/about.component';
+import { CvComponent } from './views/cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
