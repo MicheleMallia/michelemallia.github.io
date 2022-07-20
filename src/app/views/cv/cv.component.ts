7957c8aae81830bbd8d2deb7c18efe64ada29989
MicheleMallia <mallia.michele@gmail.com>
2022-07-20 14:36:42 +0200

    location and industry

--- src/app/views/cv/cv.component.ts
+++ src/app/views/cv/cv.component.ts
@@ -1,6 +1,6 @@
 import {Title} from "@angular/platform-browser";
 import { Component, OnInit } from '@angular/core';
-import { faBriefcase, faGraduationCap, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase, faGraduationCap, faLocationDot, faUserGraduate, faIndustry } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cv',
@@ -12,6 +12,8 @@ export class CvComponent implements OnInit {
   faBriefcase = faBriefcase;
   faGraduationCap = faGraduationCap;
   faLocationDot = faLocationDot;
+  faUserGraduate = faUserGraduate;
+  faIndustry = faIndustry;
   
   constructor(private titleService : Title) { 
     this.titleService.setTitle('CV - Michele Mallia')
